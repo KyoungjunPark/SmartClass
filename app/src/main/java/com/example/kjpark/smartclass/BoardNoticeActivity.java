3b25ff3df20c282c88fbc2959ff5caa62cd3e35a
parkkj <kjpark@uclab.re.kr>
2015-11-17 01:57:48 +0900

    add notice edit activity

--- app/src/main/java/com/example/kjpark/smartclass/BoardNoticeActivity.java
+++ app/src/main/java/com/example/kjpark/smartclass/BoardNoticeActivity.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2015. Kyoungjun Park. All Rights Reserved.
- */
-
 package com.example.kjpark.smartclass;
 
 import android.os.Bundle;
@@ -11,53 +7,52 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class ProfileActivity extends AppCompatActivity {
+/**
+ * Created by parkk on 2015-11-17.
+ */
+public class BoardNoticeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_board_notice);
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
-        toolbar.setTitle(R.string.profile);
+
         setToolbar();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_user, menu);
-        menu.removeItem(R.id.action_user);
-        menu.removeItem(R.id.action_write);
+        getMenuInflater().inflate(R.menu.menu_notice_board, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
         return super.onOptionsItemSelected(item);
     }
 
-    private void setToolbar(){
+    private void setToolbar()
+    {
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationIcon(R.drawable.left_arrow);
+
+        toolbar.setNavigationIcon(R.drawable.ic_close);
+
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //if user typing any words.. then handling it!
                 onBackPressed();
             }
         });
     }
+
 
 }
--- app/src/main/java/com/example/kjpark/smartclass/JoinActivity.java
+++ app/src/main/java/com/example/kjpark/smartclass/JoinActivity.java
@@ -30,12 +30,6 @@ public class JoinActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
