60291d8c616899940828a1ee067341bb7e5f83b5
parkkj <kjpark@uclab.re.kr>
2015-11-20 01:43:11 +0900

    implement login process

---
+++ app/src/main/java/com/example/kjpark/smartclass/ConnectServer.java
@@ -0,0 +1,35 @@
+package com.example.kjpark.smartclass;
+
+import android.os.AsyncTask;
+
+import java.net.HttpURLConnection;
+
+/**
+ * Created by parkk on 2015-11-20.
+ */
+public class ConnectServer {
+    private AsyncTask<String, Void, Boolean> task;
+    private String token;
+
+    private static final ConnectServer instance = new ConnectServer();
+
+    private ConnectServer(){}
+    public static ConnectServer getInstance(){
+        return instance;
+    }
+    public void setAsncTask(AsyncTask<String, Void, Boolean> task) {
+        this.task = task;
+    }
+    public void setToken(String token){this.token = token;}
+    public void execute(){
+        this.task.execute();
+    }
+
+    public HttpURLConnection setHeader(HttpURLConnection con){
+        con.setRequestProperty("Accept-Language", "ko-kr,ko;q=0.8,en-us;q=0.5,en;q=0.3");
+        con.setRequestProperty("token",this.token);
+
+        return con;
+    }
+}
+
--- app/src/main/java/com/example/kjpark/smartclass/LoginActivity.java
+++ app/src/main/java/com/example/kjpark/smartclass/LoginActivity.java
@@ -4,21 +4,47 @@
 
 package com.example.kjpark.smartclass;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView emailTextView;
     TextView passwordTextView;
     TextView newAccountButton;
+
+    EditText emailEditText;
+    EditText passwordEditText;
+
+    Button loginButton;
+
+    public static final int REQUEST_CODE_MAIN = 1001;
+    public static final int REQUEST_CODE_JOIN = 1002;
+    public static final int LOGIN_PERMITTED = 200;
+    public static final int LOGIN_DENIED = 404;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +54,92 @@ public class LoginActivity extends AppCompatActivity {
         emailTextView = (TextView)findViewById(R.id.emailTextView);
         passwordTextView = (TextView)findViewById(R.id.passwordTextView);
         newAccountButton = (Button)findViewById(R.id.newAccountButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
 
         //fonts setting
         Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/NanumPen.ttf");
         emailTextView.setTypeface(typeface);
         passwordTextView.setTypeface(typeface);
         newAccountButton.setTypeface(typeface);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = emailEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                ConnectServer.getInstance().setAsncTask(new AsyncTask<String, Void, Boolean>() {
+                    private Boolean isLoginPermitted = false;
+                    private String requestMessage;
+
+                    @Override
+                    protected Boolean doInBackground(String... params) {
+                        URL obj = null;
+                        try {
+                            obj = new URL("http://165.194.104.22:5000/login");
+                            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                            //implement below code if token is send to server
+                            con = ConnectServer.getInstance().setHeader(con);
+
+                            con.setDoOutput(true);
+
+                            String parameter = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+                            parameter += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+                            OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                            wr.write(parameter);
+                            wr.flush();
+
+                            BufferedReader rd = null;
+
+                            if (con.getResponseCode() == LOGIN_PERMITTED) {
+                                // 로그인 성공
+
+                                rd = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                                String token = rd.readLine();
+                                ConnectServer.getInstance().setToken(token);
+
+
+                                isLoginPermitted = true;
+                                //userSerialNumber = "RA-SP-BERRY-VERY-GOOD";
+                                Log.d("---- success ----", token);
+
+
+                            } else {
+                                // 로그인 실패
+                                rd = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+                                isLoginPermitted = false;
+                                requestMessage = rd.readLine();
+                                Log.d("---- failed ----", String.valueOf(rd.readLine()));
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Boolean aBoolean) {
+                        if (isLoginPermitted) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivityForResult(intent, REQUEST_CODE_MAIN);
+                            finish();
+                        } else {
+                            AlertDialog dialog = createDialogBox(requestMessage);
+                            dialog.show();
+                            emailTextView.setText("");
+                            passwordTextView.setText("");
+                        }
+
+                    }
+                });
+                ConnectServer.getInstance().execute();
+            }
+        });
 
     }
 
@@ -57,10 +163,89 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void onLoginButtonClicked(View v){
-        //login process
+        /*
+        ConnectServer.getInstance().setAsncTask(new AsyncTask<String, Void, Boolean>() {
+            private Boolean isLoginPermitted;
+            private String requestMessage;
 
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+            @Override
+            protected Boolean doInBackground(String... params) {
+                URL obj = null;
+                try {
+                    obj = new URL("http://165.194.104.22:5000/login");
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                    //implement below code if token is send to server
+                    con = ConnectServer.getInstance().setHeader(con);
+
+                    con.setDoOutput(true);
+
+                    String parameter = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+                    parameter += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+                    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+                    wr.write(parameter);
+                    wr.flush();
+
+                    BufferedReader rd = null;
+
+                    if (con.getResponseCode() == LOGIN_PERMITTED) {
+                        // 로그인 성공
+
+                        rd = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+                        String token = rd.readLine();
+                        ConnectServer.getInstance().setToken(token);
+
+
+                        isLoginPermitted = true;
+                        //userSerialNumber = "RA-SP-BERRY-VERY-GOOD";
+                        Log.d("---- success ----", token);
+
+
+                    } else {
+                        // 로그인 실패
+                        rd = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+                        isLoginPermitted = false;
+                        requestMessage = rd.readLine();
+                        Log.d("---- failed ----", String.valueOf(rd.readLine()));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Boolean aBoolean) {
+                if (isLoginPermitted) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE_MAIN);
+                    finish();
+                } else {
+                    AlertDialog dialog = createDialogBox(requestMessage);
+                    dialog.show();
+                    emailTextView.setText("");
+                    passwordTextView.setText("");
+                }
+
+            }
+        });
+        ConnectServer.getInstance().execute();
+*/
+    }
+    private AlertDialog createDialogBox(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("로그인 실패");
+
+        builder.setMessage("아이디와 비밀번호를 확인해주세요. \n" + msg + "\n");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        return dialog;
 
     }
 }
